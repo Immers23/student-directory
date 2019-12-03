d338e1b5f78c471c12cde6c71ab6a0e1f0491872
Imraan Ali <imraan91@hotmail.co.uk>
2019-12-03 18:09:27 +0000

    step7

--- directory.rb
+++ directory.rb
@@ -1,4 +1,19 @@
 def input_students
+
+months = { "" => :none,
+  "January" => :january,
+  "February" => :february,
+  "March" => :march,
+  "April" => :april,
+  "May" => :may,
+  "June" => :june,
+  "July" => :july,
+  "August" => :august,
+  "September" => :september,
+  "October" => :october,
+  "November" => :november,
+  "December" => :december}
+
 students = []
 while true do
 
@@ -8,20 +23,20 @@ while true do
 
     puts "what is the name of the person"
     name = gets.chomp
-
-    puts "what is the hobby of the student"
-    hobby = gets.chomp
-
-    puts "where is this person from"
-    location = gets.chomp
+    if name.empty?
+      name = "None"
+    end
 
     puts "what cohort is this person from"
-    cohort = gets.chomp
+    cohort = months[gets.capitalize.chomp]
+      while cohort == nil
+        puts "please type your cohort month again"
+        cohort = months[gets.capitalize.chomp]
+      end
 
-    students << {name: name, hobby: hobby, location: location, cohort: cohort}
+    students << {name: name, cohort: cohort}
 
-
-  else
+    else
     break
   end
 
@@ -37,7 +52,7 @@ end
 def print(students)
     count = 0
     until count == students.length
-    puts "#{students[count][:name]}, #{students[count][:cohort]}, #{students[count][:hobby]}, #{students[count][:location]}"
+    puts "#{students[count][:name]}, #{students[count][:cohort]}"
     count += 1
     end
 end
