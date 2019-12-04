d597796601b9bd249118dc35bbb2c55e99d64845
Imraan Ali <imraan91@hotmail.co.uk>
2019-12-04 17:52:38 +0000

    save students method created

--- directory.rb
+++ directory.rb
@@ -49,7 +49,7 @@ def print_header
   puts "-------------"
 end
 
-def print
+def print_student_lists
     count = 0
     until count == @students.length
     puts "#{@students[count][:name]}, #{@students[count][:cohort]}"
@@ -86,12 +86,13 @@ end
 def print_menu
   puts "1. Input the student"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
 def show_students
   print_header
-  print
+  print_student_lists
   print_footer
 end
 
@@ -101,6 +102,8 @@ def process(selection)
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit
     else
@@ -115,8 +118,20 @@ def interactive_menu
   end
 end
 
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of save_students
+  @students.each do |student|
+    student_data = [student[:name]], student[:cohort]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
 # nothing happens until we call the methods
 interactive_menu
+save_students
 # students = input_students
 # print_header
 # print(students)
