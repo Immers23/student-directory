413b500477930ac33f07a6af8c4a3f2482fece47
Imraan Ali <imraan91@hotmail.co.uk>
2019-12-06 15:27:43 +0000

    csv file creating

--- directory.rb
+++ directory.rb
@@ -1,4 +1,7 @@
+require "csv"
 @students = []
+@choosen_file = ""
+@filename = "students.csv"
 def input_students
 
 months = { "" => :none,
@@ -19,23 +22,23 @@ months = { "" => :none,
 while true do
 
   puts "would you like to add another student yes or no"
-  answer = gets.strip
+  answer = STDIN.gets.strip
   if answer == "yes"
 
     puts "what is the name of the person"
-    name = gets.strip
+    name = STDIN.gets.strip
     if name.empty?
       name = "None"
     end
 
     puts "what cohort is this person from"
-    cohort = months[gets.capitalize.strip]
+    cohort = months[STDIN.gets.capitalize.strip]
       while cohort == nil
         puts "please type your cohort month again"
-        cohort = months[gets.capitalize.strip]
+        cohort = months[STDIN.gets.capitalize.strip]
       end
 
-    @students << {name: name, cohort: cohort}
+    add_students(name, cohort)
 
     else
     break
@@ -62,7 +65,7 @@ end
 def print_by_cohort
 
 puts "what cohort would you like to print"
-print = gets.strip.downcase
+print = STDIN.gets.strip.downcase
 
 @students.each do |hash|
 puts hash[:name] if hash[:cohort] == print.to_sym
@@ -104,8 +107,11 @@ def process(selection)
     when "2"
       show_students
     when "3"
+      choose_file
       save_students
     when "4"
+      choose_file
+      @students = []
       load_students
     when "9"
       exit
@@ -117,31 +123,60 @@ end
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
 def save_students
   # open the file for writing
-  file = File.open("students.csv", "w")
+  CSV.open("./#{@choosen_file}", "w") do |line|
   # iterate over the array of save_students
   @students.each do |student|
     student_data = [student[:name]], student[:cohort]
-    csv_line = student_data.join(",")
-    file.puts csv_line
+    line << student_data
   end
-  file.close
+  puts "students have been saved successfully"
+end
 end
 
-def load_students
-  file = File.open("students.csv", "r")
-  file.readlines.each do |line|
-  name, cohort = line.chomp.split(',')
-    @students << {name: name, cohort: cohort.to_sym}
+def default_load
+  filename = ARGV.first
+  if filename.nil?
+  puts "Load default file: students.csv"
+  load_students(@filename)
+  elsif File.exists?(filename) # if it exists
+    load_students(filename)
+     puts "Loaded #{@students.count} from #{filename}"
+  else # if it doesn't exist
+    puts "Sorry, #{filename} doesn't exist."
+    exit # quit the program
   end
-  file.close
 end
+
+def load_students(filename = @choosen_file)
+  CSV.foreach("./#{filename}") do |row|
+    name, cohort = row
+    add_students(name, cohort)
+  end
+  puts "Students have been loaded successfully"
+end
+
+
+def add_students(name, cohort)
+  @students << {name: name, cohort: cohort.to_sym}
+end
+
+def choose_file
+  puts "which file do you want to load from?"
+  @choosen_file = STDIN.gets.chomp
+  if @choosen_file.nil? || !File.exists?(@choosen_file)
+    puts "File does not exist using file #{@filename}"
+    @choosen_file = @filename
+  end
+end
+
 # nothing happens until we call the methods
+default_load
 interactive_menu
 # save_students
 # load_students
